bc01f34da954eb81c0ed1a78f6522a9718098ac6
joedluis <joedluis@gmail.com>
2026-06-28 16:41:02 -0500

    feat: implementar metodo guardarProducto en ProductoService

--- demo/src/main/java/com/example/demo/service/ProductoService.java
+++ demo/src/main/java/com/example/demo/service/ProductoService.java
@@ -1,4 +1,20 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Producto;
+import com.example.demo.repository.ProductoRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductoService {
+
+    private final ProductoRepository productoRepository;
+
+    public ProductoService(ProductoRepository productoRepository) {
+        this.productoRepository = productoRepository;
+    }
+
+    public Producto guardarPoducto(Producto producto) {
+        return productoRepository.save(producto);
+    }
+
 }
