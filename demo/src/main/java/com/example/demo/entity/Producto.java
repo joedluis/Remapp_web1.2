9a40185fc3a45f8c57c8f262d7a74084fe49e42e
joedluis <joedluis@gmail.com>
2026-06-28 15:18:27 -0500

    feat: crear repositorio Producto con JpaRepository

--- demo/src/main/java/com/example/demo/entity/Producto.java
+++ demo/src/main/java/com/example/demo/entity/Producto.java
@@ -7,6 +7,8 @@ import java.math.BigDecimal;
 @Entity
 @Table(name = "productos")
 public class Producto {
+
+    // ===== ATRIBUTOS =====
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_producto")
@@ -18,5 +20,53 @@ public class Producto {
     @Column(name = "precio",nullable = false, precision = 10, scale = 2)
     private BigDecimal precio;
 
+    @Column(name = "stock", nullable = false)
+    private Integer stock;
 
+    // ===== CONSTRUCTORES =====
+
+    public Producto() {
+
+    }
+
+    public Producto(String nombre, BigDecimal precio, Integer stock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    // ===== GETTERS Y SETTERS =====
+
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
 }
--- demo/src/main/java/com/example/demo/repository/ProductoRepository.java
+++ demo/src/main/java/com/example/demo/repository/ProductoRepository.java
@@ -3,7 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.entity.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductoRepository extends JpaRepository<Producto, Long > {
+public interface ProductoRepository extends JpaRepository<Producto, Integer > {
 }
 
 
