88701844123c1a025c99d28f23090b44c0e05fd7
joedluis <joedluis@gmail.com>
2026-06-29 15:06:45 -0500

    feat: agregar endpoint para crear productos con la peticion HTTP POST

--- demo/src/main/java/com/example/demo/controller/ProductoController.java
+++ demo/src/main/java/com/example/demo/controller/ProductoController.java
@@ -1,4 +1,31 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Producto;
+import com.example.demo.service.ProductoService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// ====== Anotaciones =====
+@RestController
+@RequestMapping("/productos")
+
+// ======
 public class ProductoController {
+
+    private final ProductoService productoService;
+
+    public ProductoController(ProductoService productoService) {
+        this.productoService = productoService;
+    }
+
+    @PostMapping
+    public Producto guardarProducto(@RequestBody Producto producto) {
+        return productoService.guardarProducto(producto);
+
+
+    }
+
+
 }
