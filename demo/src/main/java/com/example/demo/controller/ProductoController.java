a1e973a59ebee07dd41b62eea380419273a26032
joedluis <joedluis@gmail.com>
2026-06-29 15:23:45 -0500

    feat: agregar endpoint para listar productos

--- demo/src/main/java/com/example/demo/controller/ProductoController.java
+++ demo/src/main/java/com/example/demo/controller/ProductoController.java
@@ -2,10 +2,10 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Producto;
 import com.example.demo.service.ProductoService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
+import com.example.demo.entity.Producto;
 
 // ====== Anotaciones =====
 @RestController
@@ -20,11 +20,18 @@ public class ProductoController {
         this.productoService = productoService;
     }
 
+    // ===== METODOS =====
+
+    // ===== GUARDAR =====
     @PostMapping
     public Producto guardarProducto(@RequestBody Producto producto) {
         return productoService.guardarProducto(producto);
+    }
 
-
+    // ===== LISTAR ======
+    @GetMapping
+    public List<Producto> listarProductos() {
+        return productoService.listarProductos();
     }
 
 
